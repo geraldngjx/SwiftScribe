d3412f4f4f915559c49aa1724508c5d1af0ee111
Gerald <102348985+geraldngjx@users.noreply.github.com>
2023-05-22 00:20:18 +0800

    Add files via upload

--- Frontend/src/components/navigation/ProtectedRoute.js
+++ Frontend/src/components/navigation/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
-import { useAuth } from '../..AuthContext/context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useAuth();
