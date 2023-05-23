8972d61a6d114db85e513b6f7f6f43da170a3475
Gerald <102348985+geraldngjx@users.noreply.github.com>
2023-05-23 14:03:04 +0800

    updated config files

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
+module.exports = {
+  webpack: (config) => {
+    // Add a new rule to handle mp4 files
+    config.module.rules.push({
+      test: /\.mp4$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next',
+          name: 'static/media/[name].[hash].[ext]',
+        },
+      },
+    });
+
+    return config;
+  },
+};
