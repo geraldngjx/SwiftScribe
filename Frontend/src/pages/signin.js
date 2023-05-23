aff0fed8b243ede0c0ade82a54ef8b7154e6b66c
Gerald <102348985+geraldngjx@users.noreply.github.com>
2023-05-23 14:10:22 +0800

    updates the pages

--- Frontend/src/pages/signin.js
+++ Frontend/src/pages/signin.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from "next/router";
 import { useAuth } from "../context/AuthContext";
+
+import backgroundImage from '../../public/main-background.jpg';
 
 const SignIn = () => {
   const router = useRouter();
@@ -25,15 +29,34 @@ const SignIn = () => {
   };
 
   return (
-    <div className="bg-gray-200 h-screen">
+    <div 
+      className="bg-gray-200 h-screen"
+      style={{
+        backgroundImage: `url(${backgroundImage.src})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        opacity: 0.9,
+      }}
+    >
       <div className="flex justify-center items-center h-screen">
-        <div className="bg-gray-800 w-full max-w-md rounded-lg shadow-lg">
-          <div className="py-8 px-8 rounded-lg">
-            <h1 className="text-3xl font-bold mb-8 text-green-500 text-center">
+        <div className="bg-gray-900 w-full max-w-md pt-8 rounded-lg shadow-lg">
+          <div className="flex justify-center p-4">
+          <Image
+                  src="/logo.png"
+                  alt="SwiftScribe Logo"
+                  className="dark:invert"
+                  width={300}
+                  height={200}
+                  priority
+                />
+          </div>
+          <div className="px-8 pb-8 rounded-lg">
+            <h1 className="text-2xl font-bold mb-8 text-green-500 text-center">
               Welcome back!
             </h1>
             <form onSubmit={handleLogin}>
-              <div className="mb-4">
+              <div className="mb-3">
                 <label
                   htmlFor="email"
                   className="block text-gray-400 font-bold mb-2"
@@ -88,12 +111,12 @@ const SignIn = () => {
               <p className="text-gray-400 text-sm font-medium">
                 New to our platform?
               </p>
-              <a
+              <Link
                 href="/signup"
                 className="text-green-500 hover:text-green-600 font-bold"
               >
                 Create an account
-              </a>
+              </Link>
             </div>
           </div>
         </div>
