34dfe8196dedaf9fa1affac3f70c66cd02579984
ryantzr1 <dreisprung1821@gmail.com>
2023-05-26 16:31:10 +0800

    testing

--- Frontend/ffmpeg_install.sh
+++ Frontend/ffmpeg_install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-if [ "$FFMPEG_INSTALL" == "true" ]; then
-  echo "Installing ffmpeg..."
+echo "Installing ffmpeg..."
 
-  # Check the operating system
-  if [ "$VERCEL_ENV" == "production" ]; then
-     echo "Detected Linux environment (Vercel production)"
-    sudo apt update
-    sudo apt install ffmpeg -y
-  elif [ "$VERCEL_ENV" == "development" ]; then
-     echo "Detected macOS environment (Vercel development)"
-    brew install ffmpeg
-  else
-    echo "Unknown environment"
-    exit 1
-  fi
+# Check the operating system
+if [ "$VERCEL_ENV" == "production" ]; then
+  echo "Detected Linux environment (Vercel production)"
+  sudo apt update
+  sudo apt install ffmpeg -y
+elif [ "$VERCEL_ENV" == "development" ]; then
+  echo "Detected macOS environment (Vercel development)"
+  brew install ffmpeg
+else
+  echo "Unknown environment"
+  exit 1
 fi
