50368da793669d06ca3f4012f3b816ab2a8471d7
ryantzr1 <dreisprung1821@gmail.com>
2023-06-14 12:53:48 +0800

    Integrate Cypress tests with GitHub Actions

--- Frontend/cypress.config.js
+++ Frontend/cypress.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  integrationFolder: "cypress/e2e",
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 });
