698bc43e3b85c0f9b8291852b33d1ddbc4e03ea7
ryantzr1 <dreisprung1821@gmail.com>
2023-07-08 22:06:47 +0800

    Fixing small issue with navbar test case

--- Frontend/cypress/e2e/navbar.cy.js
+++ Frontend/cypress/e2e/navbar.cy.js
@@ -1,4 +1,4 @@
-describe("Upload Page", () => {
+describe("Navbar", () => {
   beforeEach(() => {
     cy.visit("https://swift-scribe.vercel.app/signin");
 
@@ -6,22 +6,30 @@ describe("Upload Page", () => {
     cy.get('input[type="password"]').type("testing12345");
 
     cy.get('button[type="submit"]').click();
+
+    // Check if the user is authenticated and home page is loaded
+    cy.url().should("include", "/");
+    cy.get(".logo").should("be.visible");
   });
 
   it("Validates Navbar Links", () => {
+    // Click on Dashboard
+    cy.get(".nav-links").contains("Dashboard").click();
+    cy.url().should("include", "/");
+
+    cy.wait(5000);
     cy.get(".nav-links").contains("Upload Media").click();
+
     cy.url().should("include", "/upload");
 
-    cy.visit("/");
+    cy.wait(5000);
 
     cy.get(".nav-links").contains("Manage Files").click();
     cy.url().should("include", "/manage");
   });
 
   it("Validates Sign out functionality", () => {
-    cy.get(
-      ".w-full.rounded-md.bg-gray-700.text-gray-200.hover\\:bg-gray-600.py-2"
-    ).click();
+    cy.get("button").contains("Sign out").click();
 
     cy.url().should("include", "/login");
   });
