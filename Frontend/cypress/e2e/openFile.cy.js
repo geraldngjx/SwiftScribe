5c639d62821260cfbaadb3e9b1cf18ea55703428
ryantzr1 <dreisprung1821@gmail.com>
2023-07-08 22:13:24 +0800

    Fixing small issue with openFile test case

--- Frontend/cypress/e2e/openFile.cy.js
+++ Frontend/cypress/e2e/openFile.cy.js
@@ -10,36 +10,36 @@ describe("FileRow", () => {
     cy.get('button[type="submit"]').click();
 
     cy.visit("https://swift-scribe.vercel.app/upload");
-
-    const fileName = "./cypress/fixtures/5607.mp4"; //this is found in the fixtures folder
-    const uploadedFileName = "5607.mp4"; //this is found in the text box in our upload media page
-    cy.get(".video-upload-container .panel button").contains("Upload").click();
-
-    cy.get("input[type=file]")
-      .invoke("attr", "style", "display: block")
-      .selectFile(fileName);
-
-    cy.get(".text-transcription-container input[type=text]").should(
-      "have.value",
-      uploadedFileName
-    );
-    cy.get(".text-transcription-container button").contains("Save").click();
-
-    cy.visit("https://swift-scribe.vercel.app/");
   });
 
-  it("should delete a file when the Delete button is clicked", () => {
-    const onDelete = cy.stub().as("onDelete");
+  const fileName = "./cypress/fixtures/5607.mp4"; //this is found in the fixtures folder
+  const uploadedFileName = "5607.mp4"; //this is found in the text box in our upload media page
+  cy.get(".video-upload-container .panel button").contains("Upload").click();
 
-    cy.get("button").contains("Delete").click();
+  cy.get("input[type=file]")
+    .invoke("attr", "style", "display: block")
+    .selectFile(fileName);
 
-    cy.get(".fixed.inset-0").should("be.visible");
+  cy.get(".text-transcription-container input[type=text]").should(
+    "have.value",
+    uploadedFileName
+  );
+  cy.get(".text-transcription-container button").contains("Save").click();
 
-    cy.get(".fixed.inset-0 button")
-      .contains("Open")
-      .click()
-      .then(() => {
-        expect(onDelete);
-      });
-  });
+  cy.visit("https://swift-scribe.vercel.app/");
+});
+
+it("should delete a file when the Delete button is clicked", () => {
+  const onDelete = cy.stub().as("onDelete");
+
+  cy.get("button").contains("Delete").click();
+
+  cy.get(".fixed.inset-0").should("be.visible");
+
+  cy.get(".fixed.inset-0 button")
+    .contains("Open")
+    .click()
+    .then(() => {
+      expect(onDelete);
+    });
 });
