befb21e0205810311b4ff2636eae1995d7e3705c
Gerald Ng <geraldngjx@gmail.com>
2023-07-16 21:35:45 +0800

    modified api request sending process

--- Frontend/src/pages/api/temp.js
+++ Frontend/src/pages/api/temp.js
@@ -9,46 +9,45 @@ export default async function extractTranscript(req, res) {
     await connectMongo();
     console.log("CONNECTED TO MONGO");
 
-    const { text_id, purpose, duration } = req.body;
+    const { text_id, purpose } = req.body;
 
     const temp = await Temp.findOne({ text_id });
 
     if (!temp) {
       return res.status(404).json({ error: "Temp object not found" });
     }
+    
+    if (purpose == "Transcription") {
+        if (temp.transcript === "__EMPTY__") {
+            return res.status(400).json({ error: "Transcript is empty" });
+        } else {
+            const text = temp.transcript;
 
-    let text = purpose === "Transcription" ? temp.transcript : temp.summary;
+            // You can perform additional actions with the extracted transcript here
+        
+            return res.json({ text });
+        }
+    } else {
+        if (temp.summary === "__EMPTY__") {
+            if (temp.transcript === "__EMPTY__") {
+                return res.status(400).json({ error: "Transcript and Summary is empty" });
+            } else {
+                const text = temp.transcript;
 
-    const startTime = new Date().getTime(); // Start time in milliseconds
-    let currentTime = startTime; // Current time starts with the start time
+                // You can perform additional actions with the extracted transcript here
+            
+                return res.json({ text });
+            }
+        } else {
+            const text = temp.summary;
 
-    // Start the loop with an interval of 1 minute
-    const interval = setInterval(async () => {
-      const updatedTemp = await Temp.findOne({ text_id });
-      if (!updatedTemp) {
-        clearInterval(interval); // Stop the loop
-        return res.status(500).json({ error: "Failed to find updated Temp object" });
-      }
-
-      text = purpose === "Transcription" ? updatedTemp.transcript : updatedTemp.summary;
-
-      // Check if the text field is still empty
-      if (text !== "__EMPTY__") {
-        clearInterval(interval); // Stop the loop
-        // You can perform additional actions with the extracted text here
-        return res.json({ text });
-      }
-
-      currentTime = new Date().getTime(); // Update the current time
-
-      // Check if the duration from the start time exceeds the video duration x 2
-      if (currentTime - startTime > duration * 2) {
-        clearInterval(interval); // Stop the loop
-        return res.status(500).json({ error: "Extraction timed out" });
-      }
-    }, 60000); // Interval of 1 minute
+            // You can perform additional actions with the extracted summary here
+        
+            return res.json({ text });
+        }
+    }
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "An error occurred" });
-  }
+  } 
 }
