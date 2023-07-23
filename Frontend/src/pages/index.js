f93a678a5291c7ba78ac3c4eb7fd9f4527f89b82
Gerald Ng <geraldngjx@gmail.com>
2023-07-23 19:30:15 +0800

    added loading spinner

--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -8,6 +8,7 @@ function Dashboard() {
   const [fileCount, setFileCount] = useState(0);
   const [storageCount, setStorageCount] = useState(0);
   const { user } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchFiles = async () => {
@@ -22,10 +23,10 @@ function Dashboard() {
           }),
         });
         const data = await response.json();
-        console.log("DATA: " + data);
         setLogs(data.files);
         setFileCount(data.files.length);
         setStorageCount(data.totalStorage);
+        setIsLoading(false); // Set loading to false once data is fetched
       } catch (error) {
         console.error("Error fetching files:", error);
       }
@@ -36,7 +37,6 @@ function Dashboard() {
 
   const deleteFile = async (fileId) => {
     try {
-      console.log(fileId);
       await fetch("/api/delete", {
         method: "POST",
         headers: {
@@ -53,18 +53,41 @@ function Dashboard() {
     }
   };
 
-  console.log("logs:", logs);
-
   return (
     <main className="h-screen">
       <h1 className="text-4xl font-bold p-8">My Dashboard</h1>
       <div className="flex justify-between pl-8 pr-12">
         <StatisticsContainer title="Files in Database" value={fileCount.toString()} />
-        <StatisticsContainer title="Total Storage Used (MB)" value={fileCount.toString() * 0.02} />
+        <StatisticsContainer title="Total Storage Used (MB)" value={(fileCount * 0.02).toFixed(2)} />
       </div>
-      <div className="bg-white p-8">
-        <FileContainer logs={logs} onDelete={deleteFile} />
-      </div>
+      {isLoading ? (
+        <div className="p-16 flex items-center justify-center">
+          <svg
+            className="animate-spin h-8 w-8 text-gray-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      ) : (
+        <div className="bg-white p-8">
+          <FileContainer logs={logs} onDelete={deleteFile} />
+        </div>
+      )}
     </main>
   );
 }
--- Frontend/src/pages/manage.js
+++ Frontend/src/pages/manage.js
@@ -6,6 +6,7 @@ const FileManagementPage = () => {
   const [logs, setLogs] = useState([]);
   const [fileCount, setFileCount] = useState(0);
   const [searchQuery, setSearchQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
   const { user } = useAuth();
 
   useEffect(() => {
@@ -23,6 +24,7 @@ const FileManagementPage = () => {
         const data = await response.json();
         setLogs(data.files);
         setFileCount(data.files.length);
+        setIsLoading(false); // Set loading to false once files are received
       } catch (error) {
         console.error("Error fetching files:", error);
       }
@@ -70,7 +72,32 @@ const FileManagementPage = () => {
         />
       </div>
       <div className="bg-white p-8 h-[calc(100vh-150px)] overflow-y-auto">
-        <FileContainer logs={filteredLogs} onDelete={deleteFile} />
+        {isLoading ? (
+          <div className="flex items-center justify-center">
+            <svg
+              className="animate-spin h-8 w-8 text-gray-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>
+        ) : (
+          <FileContainer logs={filteredLogs} onDelete={deleteFile} />
+        )}
       </div>
     </main>
   );
